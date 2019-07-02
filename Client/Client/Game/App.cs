73cbc84d33cfa1c8fbb542015b4b245e43b478af
Jeffery Myers <JeffM2501@gmail.com>
2019-07-01 21:52:33 -0700

    fix sound when not in debug. move over to a non forms based framework

--- Client/Client/Game/App.cs
+++ Client/Client/Game/App.cs
@@ -8,6 +8,8 @@ using Urho;
 using Urho.Resources;
 using Urho.Actions;
 
+using Urho.Audio;
+
 namespace Client.Game
 {
     public class App : Application
@@ -21,6 +23,10 @@ namespace Client.Game
 
         protected override void Start()
         {
+            base.Start();
+            Input.SetMouseMode(MouseMode.Absolute);
+            Input.SetMouseVisible(true);
+
             Menus.Stack.Setup(this);
 
             SetupScene();
@@ -43,6 +49,8 @@ namespace Client.Game
             World.CreateComponent<Octree>();
         }
 
+        private Node musicNode = null;
+
         public void SetupMainMenu()
         {
             World.Clear();
@@ -54,10 +62,21 @@ namespace Client.Game
 
             Menus.Stack.ClearAll();
 
-             var main = new Menus.Main();
-             main.StartGame += Main_StartGame;
-             main.Quit += Main_Quit;
-             Menus.Stack.Push(main);
+            var main = new Menus.Main();
+            main.StartGame += Main_StartGame;
+            main.Quit += Main_Quit;
+            Menus.Stack.Push(main);
+
+            var music = ResourceCache.GetSound("Sounds/Ambient/425368__soundholder__ambient-meadow-near-forest-single-bird-and-eurasian-cranes-in-background-stereo-xy-mk012_01.ogg");
+            music.Looped = true;
+            musicNode = World.CreateChild("Music");
+            SoundSource musicSource = musicNode.CreateComponent<SoundSource>();
+             musicSource.Gain = 0.25f;
+          //  musicSource.Attenuation = 1;
+            // Set the sound type to music so that master volume control works correctly
+            musicSource.SetSoundType(SoundType.Music.ToString());
+            musicSource.Play(music);
+
         }
 
         private void Main_Quit(object sender, EventArgs e)
--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using Urho;
+using Urho.Desktop;
 
 namespace Client
 {
@@ -14,9 +20,55 @@ namespace Client
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GameWindow());
+            Config.Current = Config.Load();
+
+            string dataPath = Config.Current.AssetFolder;
+
+            if (dataPath == string.Empty || !Directory.Exists(dataPath))
+                dataPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "assets");
+
+            if (!Directory.Exists(dataPath))
+            {
+                MessageBox.Show(ClientResources.NoAssetPathError.Replace("$F", dataPath), ClientResources.NoAssetPathErrorTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (Directory.Exists(dataPath))
+            {
+                DesktopUrhoInitializer.AssetsDirectory = dataPath;
+
+                ApplicationOptions options = new ApplicationOptions("Data");
+                options.Orientation = ApplicationOptions.OrientationType.Landscape;
+                options.ResizableWindow = false;
+                options.WindowedMode = Config.Current.WinType != Config.WindowTypes.FullScreen;
+                if (Config.Current.WinType == Config.WindowTypes.Window)
+                {
+                    options.Width = Config.Current.WindowBounds.Width;
+                    options.Height = Config.Current.WindowBounds.Height;
+                }
+                options.Multisampling = 8;
+                options.LimitFps = true;
+                options.NoSound = false;
+              //  options = null;
+                Game.App app = new Game.App(options);
+                var exitCode = app.Run();
+            }
+        }
+
+
+        private static bool PointIsVisible(Point p)
+        {
+            foreach (var s in Screen.AllScreens)
+            {
+                if (p.X < s.Bounds.Right && p.X > s.Bounds.Left && p.Y > s.Bounds.Top && p.Y < s.Bounds.Bottom)
+                    return true;
+            }
+            return false;
+        }
+
+        private static bool RectIsVisible(Rectangle f)
+        {
+            return PointIsVisible(new Point(f.Left, f.Top)) && PointIsVisible(new Point(f.Right, f.Top)) && PointIsVisible(new Point(f.Left, f.Bottom)) && PointIsVisible(new Point(f.Right, f.Bottom));
         }
     }
 }
